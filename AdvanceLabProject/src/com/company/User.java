ae8c0862a512953ba09769c162bccd5ec69b927e
ALPHA-04\Academy-04 <ivo_stefanov@gbg.bg>
2022-01-10 20:07:37 +0200

    added full name

--- AdvanceLabProject/src/com/company/User.java
+++ AdvanceLabProject/src/com/company/User.java
@@ -1,32 +1,24 @@
 package com.company;
 
 public class User {
-    private String firstName;
-    private String lastName;
+    private String fullName;
     private String email;
     private String password;
 
     public User(String firstName, String lastName, String email, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.fullName = fullName;
         this.email = email;
         this.password = password;
     }
 
-    public String getFirstName() {
-        return firstName;
+
+
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public String getEmail() {
