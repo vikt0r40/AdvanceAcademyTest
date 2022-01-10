e5527fbb5d9bd01a6acca6ebedec14be579d364b
ALPHA-04\Academy-04 <ivo_stefanov@gbg.bg>
2022-01-10 21:07:27 +0200

    - fixes

--- AdvanceLabProject/src/com/company/User.java
+++ AdvanceLabProject/src/com/company/User.java
@@ -5,29 +5,19 @@ public class User {
     private String email;
     private String password;
 
-<<<<<<< HEAD
-    public User(String firstName, String lastName, String email, String password) {
-=======
+
     public User(String fullName, String email, String password) {
->>>>>>> main
+
         this.fullName = fullName;
         this.email = email;
         this.password = password;
     }
 
-<<<<<<< HEAD
-
 
     public String getFullName() {
         return fullName;
     }
 
-=======
-    public String getFullName() {
-        return fullName;
-    }
-
->>>>>>> main
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
