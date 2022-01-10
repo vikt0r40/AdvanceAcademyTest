c3d0c58bed16caa91d6bc31cda9d371fefffc86c
mrwinner04 <dani.gelov@gmail.com>
2022-01-10 19:46:43 +0200

    remove change email

--- AdvanceLabProject/src/com/company/User.java
+++ AdvanceLabProject/src/com/company/User.java
@@ -33,10 +33,6 @@ public class User {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
