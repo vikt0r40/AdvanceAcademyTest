d16b42539cf0309f34eb0a818d0ed22b9446b37a
Pavlin Pavlov <paffkata_555@abv.bg>
2022-01-10 20:32:15 +0200

    - add new user

--- AdvanceLabProject/src/com/company/Main.java
+++ AdvanceLabProject/src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        User user = new User("Viktor", "Todorov","aaa@aa.bg","1234");
+        User user = new User("Viktor Todorov", "aaa@aa.bg","1234");
+        User user99 = new User("Pavlin Pavlov", "paf@aa.bg","5555");
         Ad ad = new Ad("iPhone XS 64GB", "2000", user);
 
 
