74b54f7b0cf3c331f69eff8f76ee75f4e2bddf40
knepbg <knep.87@gmail.com>
2022-01-10 20:22:21 +0200

    add new user

--- AdvanceLabProject/src/com/company/Main.java
+++ AdvanceLabProject/src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         User user = new User("Viktor Todorov","aaa@aa.bg","1234");
+        User user2 = new User("Filip Jordanov","bbbb@aa.bg","123456789");
         Ad ad = new Ad("iPhone XS 64GB", "2000", user);
 
 
