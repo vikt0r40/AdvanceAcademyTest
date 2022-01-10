e274440e8075df3380ff3ee8a5d73d1c3f971f61
knepbg <knep.87@gmail.com>
2022-01-10 21:01:09 +0200

    new users

--- AdvanceLabProject/src/com/company/Main.java
+++ AdvanceLabProject/src/com/company/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
         User user = new User("Viktor Todorov","aaa@aa.bg","1234");
         User user2 = new User("Filip Jordanov","bbbb@aa.bg","123456789");
+        User user3 = new User("Filip Jordanov","bbbb@aa.bg","123456789");
+        User user4 = new User("Filip Jordanov","bbbb@aa.bg","123456789");
+
         Ad ad = new Ad("iPhone XS 64GB", "2000", user);
 
 
