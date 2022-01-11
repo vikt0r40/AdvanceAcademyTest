71745a23083274d5d65d091523a1d5f27aa1a596
xMrShadyx <daredevil91138@gmail.com>
2022-01-11 12:58:42 +0200

    Test Commit

--- AdvanceLabProject/src/com/company/Main.java
+++ AdvanceLabProject/src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
         User user = new User("Viktor Todorov","aaa@aa.bg","1234");
         User user2 = new User("Filip Jordanov","bbbb@aa.bg","123456789");
+        User user3 = new User("Hyusein Hyusein", "vvvv@vvv.bg", "696969");
         Ad ad = new Ad("iPhone XS 64GB", "2000", user);
+        Ad newAd = new Ad("Redmi Note 10 5G", "500", user);
 
 
         System.out.println(ad);
